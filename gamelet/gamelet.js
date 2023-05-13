4ffaa2e063d997da4f2e452c9f13687753afbb19
Peter-Kibet <peter.kibet@hotmail.com>
2023-05-13 09:00:00 +0300

    Add comments to the code

--- gamelet/gamelet.js
+++ gamelet/gamelet.js
@@ -1,25 +1,29 @@
-const ball = document.getElementById('ball');
-document.addEventListener('keydown', handleKeyPress);
-let position = 0;
+const ball = document.getElementById("ball");
+document.addEventListener("keydown", handleKeyPress);
+let position = 0; //sets initial position of ball
 
+/*
+Handle key press events
+responds to certain key presses by updating position
+*/
 function handleKeyPress(e) {
-    if (e.code === 'ArrowRight') {
-        position += 10;
-        ball.style.left = position + 'px';
-    }
-    if (e.code === 'ArrowLeft') {
-        position -= 10;
-        ball.style.left = position + 'px';
-    }
-    if (position < 0) {
-        position = 0;
-    }
-    refresh();
+  if (e.code === "ArrowRight") {
+    position += 10;
+    ball.style.left = position + "px";
+  }
+  if (e.code === "ArrowLeft") {
+    position -= 10;
+    ball.style.left = position + "px";
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  refresh();
 }
 function refresh() {
-    if (position > 250) {
-        ball.style.backgroundColor = 'red';
-    } else {
-        ball.style.backgroundColor = 'green';
-    }
+  if (position > 250) {
+    ball.style.backgroundColor = "red";
+  } else {
+    ball.style.backgroundColor = "green";
+  }
 }
